207670fba46bf63f40416153c212f44b68d6514d
Sebastian Hönel <development@hoenel.net>
2018-08-12 22:50:41 +0200

    Import and export new symbols and formatters in index.js and bump version to v2.7.0

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ require('./docs.js');
 
 const LogLevel = require('./Logging/LogLevel')
 , { BaseLogger, BaseScope, BaseLogEvent,
-  symbolBeginScope, symboleEndScope, symbolMessageLogged } = require('./Logging/BaseLogger')
+  symbolBeginScope, symboleEndScope, symbolMessageLogged,
+  symbolBeforeLogMessage, symbolAfterLogMessage,
+  defaultErrorFormatter, defaultEventFormatter,
+  defaultFormatter, defaultValueFormatter } = require('./Logging/BaseLogger')
 , { ColoredConsoleLogger} = require('./Logging/ColoredConsoleLogger')
 , { DevNullLogger } = require('./Logging/DevNullLogger')
 , { InMemoryLogger, InMemoryLogMessage, MsgSortOrder } = require('./Logging/InMemoryLogger')
@@ -15,6 +18,9 @@ module.exports = Object.freeze({
   LogLevel,
   BaseLogger, BaseScope, BaseLogEvent,
   symbolBeginScope, symboleEndScope, symbolMessageLogged,
+  symbolBeforeLogMessage, symbolAfterLogMessage,
+  defaultErrorFormatter, defaultEventFormatter,
+  defaultFormatter, defaultValueFormatter,
   ColoredConsoleLogger,
   DevNullLogger,
   InMemoryLogger, InMemoryLogMessage, MsgSortOrder,
